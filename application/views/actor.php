fe0fbd05201c01fdefae2e4b46c66d3f8d35cd7f
angusl2004 <angusl2004@gmail.com>
2018-01-18 15:23:24 -0800

    formatted the actor info in actor.php

--- application/views/actor.php
+++ application/views/actor.php
@@ -3,5 +3,9 @@
                 <a href="/welcome/show/{key}">
                         <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
                 </a>
+				<br>
+				<p><b>Who:</b> {who}</p>
+				<p><b>Role:</b> {role}</p>
+				<p><b>What:</b> {what}</p>
         </div>
 </div>
--- changelog.md
+++ changelog.md
@@ -17,6 +17,7 @@ guideline:
 
 ENTRIES
 
+1/18/2018 -Formatted the actor info in actor.php -AL
 1/18/2018 -updated actor.php to display images inside anchor elements -CL
 1/18/2018 -image now displays a quote on mouseover -AL
 1/18/2018 -Quotes.php modified, bond.jpg added to public/pix -CL
